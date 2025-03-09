5c8571d9c726f92a3ed8795fae81e54bc52de57b
horinoA <horinoakiko@gmail.com>
2025-03-09 16:27:50 +0900

    first commit

---
+++ src/main/java/com/example/securityFlywayTest/SecurityFlywayTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityFlywayTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityFlywayTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityFlywayTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/securityFlywayTest/SecurityFlywayTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityFlywayTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityFlywayTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
